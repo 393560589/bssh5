2dc879ae174717827ffc5caee49c3e84ce1b8fe7
Miao <yunliangmiao@gmail.com>
2018-09-22 13:54:03 +0800

    搜索页更新

--- src/pages/SearchResult/page.js
+++ src/pages/SearchResult/page.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react'
 import styles from './index.less'
-import resultApi from '../../services/search'
+import {resultFix, resultFlow} from '../../services/search'
 import router from 'umi/router'
 
 class SearchResult extends PureComponent {
@@ -11,19 +11,24 @@ class SearchResult extends PureComponent {
       ba: '',
       post: '',
       news: '',
-      hotWords: '',
-      error: false
+      hotWords: [],
+      others: [],
+      error: false,
+      flowPage: 1,
+      keyword: ''
     }
   }
 
   componentDidMount() {
-    const {location: { query: keyword }} = router
+    const {location: { query: { keyword } }} = router
+    this.setState({keyword})
     this.fetchResult(keyword)
+    this.fetchFlowResult(keyword)
   }
 
   fetchResult = async (keyword) => {
-    const result = await resultApi({keyword})
-    console.log(result)
+    const result = await resultFix({keyword})
+    // const flowResult = await resultFlow({keyword, page})
     if (result === null || result === undefined) {
       // 搜索结果为空或请求出错
       this.setState({error: true})
@@ -31,6 +36,46 @@ class SearchResult extends PureComponent {
       const { baike, ba, news, about_post: post, hot_words: hotWords } = result
       this.setState({baike, ba, news, post, hotWords})
     }
+  }
+
+  fetchFlowResult = async (keyword) => {
+    const {flowPage: page} = this.state
+    const flowResult = await resultFlow({keyword, page})
+    if (flowResult === null || flowResult === undefined) {
+      // 搜索结果为空或请求出错
+      // this.setState({error: true})
+    } else {
+      // const { baike, ba, news, about_post: post, hot_words: hotWords } = result
+      this.setState({others: flowResult.news})
+    }
+  }
+
+  goPrev = () => {
+    const {keyword} = this.state
+    // const {location: { query: { keyword } }} = router
+    this.setState((prev => {
+      if (prev.flowPage > 1) {
+        return { flowPage: prev.flowPage - 1}
+      }
+    }))
+    this.fetchFlowResult(keyword)
+  }
+
+  goNext = () => { // 
+    // const {location: { query: { keyword } }} = router
+    const {keyword} = this.state
+    this.setState((prev => {
+      if (prev.flowPage > 1) {
+        return { flowPage: prev.flowPage - 1}
+      }
+    }))
+    this.fetchFlowResult(keyword)
+  }
+
+  search = (keyword) => {
+    this.setState({keyword, flowPage: 1})
+    this.fetchResult(keyword)
+    this.fetchFlowResult(keyword)
   }
 
   renderPrice = () => {
@@ -102,7 +147,7 @@ class SearchResult extends PureComponent {
   renderPost = () => {
     return (
       <section className="p-15 mb-8">
-        <h3>关于EOS的帖子</h3>
+        <h3>关于{this.state.keyword}的帖子</h3>
         {this.state.post && this.state.post.map(p => <p className="mb-8" key={p.id}>{`${p.post_title}[${p.board_title}]`}</p>)}
         {/* <p className="mb-8">7月31日BTC、EOS交易策略【比特币吧</p>
         <p>爆炒区块链3.0概念，EOS想超ETH (以太坊)，没那么简单!【以太坊吧】</p> */}
@@ -114,7 +159,7 @@ class SearchResult extends PureComponent {
     const { news } = this.state
     return (
       <section className="p-15 mb-8">
-        <h3>近期关于eos的相关新闻</h3>
+        <h3>近期关于{this.state.keyword}的相关新闻</h3>
         {news && news.map(n => <p className="mb-8" key={n.title}><a href={n.url}>{n.title}</a></p>)}
         {/* <p className="mb-8">号称超越ETH、吊打EOS，技术流IOST的底气到底在哪里？</p>
         <p className="mb-8">白皮说 | EOS是下一代区块链的王者还是一个百亿美元的骗局?</p>
@@ -123,34 +168,29 @@ class SearchResult extends PureComponent {
     )
   }
 
-  renderOther = () => {
+  renderOther = ({title, description, url, id}) => {
     return (
-      <section className="p-15 mb-8">
-        <h3>EOS合约新闻 EOS合约动态 EOS合约投资资讯-财链社</h3>
-        <p className="mb-8">投资家全方位为您提供EOS合约相关新闻资讯，全面解读EOS合约投资、融资、并购等动态。</p>
-        <a href="" className={styles.link}>查看更多</a>
+      <section key={id} className="p-15 mb-8">
+        <h3>{title}</h3>
+        <p className="mb-8">{description}</p>
+        <a href={url} className={styles.link}>查看更多</a>
       </section>
     )
   }
 
   renderHots = () => {
+    const { hotWords = [] } = this.state
+    const left = hotWords.slice(0, 5)
+    const right = hotWords.slice(5, 10)
     return (
       <section>
         <h2 className={styles["top-searchs"]}>搜索热点</h2>
         <div className="ds-fa pl-20">
           <ul>
-            <li>比<em>特币</em></li>
-            <li>eos</li>
-            <li>比特币</li>
-            <li>eos</li>
-            <li>比特币</li>
+            {left.map(item => <li key={item} onClick={() => this.search(item)}>{item}</li>)}
           </ul>
           <ul>
-            <li>比特币</li>
-            <li>eos</li>
-            <li>比特币</li>
-            <li>eos</li>
-            <li>比特币</li>
+            {right.map(item => <li key={item} onClick={() => this.search(item)}>{item}</li>)}
           </ul>
         </div>
       </section>
@@ -160,9 +200,9 @@ class SearchResult extends PureComponent {
   renderPagination = () => {
     return (
       <section className="mt-8 ds-f p-8">
-        <img src={require('../../assets/arrow-left.png')} alt="" className={styles.icon}/>
+        <img src={require('../../assets/arrow-left.png')} alt="" className={styles.icon} onClick={this.goPrev}/>
         <span className={styles.page}>第 1 页</span>
-        <img src={require('../../assets/arrow-right.png')} alt="" className={styles.icon}/>
+        <img src={require('../../assets/arrow-right.png')} alt="" className={styles.icon} onClick={this.goNext}/>
       </section>
     )
   }
@@ -185,10 +225,7 @@ class SearchResult extends PureComponent {
           {/* {this.renderApp()} */}
           {this.renderPost()}
           {this.renderNews()}
-          {this.renderOther()}
-          {this.renderOther()}
-          {this.renderOther()}
-          {this.renderOther()}
+          {this.state.others.map(item => this.renderOther(item))}
           {this.renderHots()}
           {this.renderPagination()}
         </div>)
