70b8fcae431519390b26c2a5855af2ae7c75e7d4
杨伟韦 <865562227@qq.com>
2018-10-07 19:49:30 +0800

    加载loading

--- src/components/HeaderBar/Header.js
+++ src/components/HeaderBar/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import style from './Index.less'
 import { Icon } from 'antd-mobile'
+import router from 'umi/router'
 
 const Header = (props) => {
     const { title = '标题', type, id } = props;
@@ -15,7 +16,7 @@ const Header = (props) => {
             <div className={style.header_center}>{ title }</div>
             <div className={style.header_right}>
             {
-                type == '3' ? <a style={{color: "#4E8CEE"}} href={"/BusinessCard?id=" + id}>生成名片</a> : null
+                type == '3' ? <span style={{color: "#4E8CEE"}} onClick={ () => router.push(`/BusinessCard?id=${id}`) }>生成名片</span> : null
             }
             </div>
     	</div>
