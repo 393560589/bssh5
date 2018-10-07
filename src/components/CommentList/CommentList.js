b7d5a873bb96d7982eb3aa625fd36498fa3b40d9
杨伟韦 <865562227@qq.com>
2018-10-07 19:31:19 +0800

    新闻点击loading

--- src/components/CommentList/CommentList.js
+++ src/components/CommentList/CommentList.js
@@ -1,31 +1,35 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import style from './CommentList.less'
 import {formatData} from '../../utils/functions'
 
+class CommentList extends PureComponent {
 
-
-const CommentList = (props) => {
-    const { data } = props;
-
-    function handleUrl(url) {
+    handleUrl = (url) => {
+        const { handleClick } = this.props;
+        handleClick();
         window.location.href = url;
     }
 
-  	return (
-    	<div className={style.comment_list}>
-            {
-                data && data.map(item => {
-                    return <div className={style.comment_cell} onClick={() => handleUrl(item.url)}>
-                                <div className={style.comment_title}>{ item && item.title }</div>
-                                <div className={style.comment_con_box}>
-                                    <div className={style.comment_time}>{ item && formatData(item.time_num) }</div>
-                                    <div className={style.comment_label}>{ item && item.resource }</div>
+    render() {
+        const { data } = this.props;
+
+        return (
+            <div className={style.comment_list}>
+                {
+                    data && data.map(item => {
+                        return <div className={style.comment_cell} onClick={() => this.handleUrl(item.url)}>
+                                    <div className={style.comment_title}>{ item && item.title }</div>
+                                    <div className={style.comment_con_box}>
+                                        <div className={style.comment_time}>{ item && formatData(item.time_num) }</div>
+                                        <div className={style.comment_label}>{ item && item.resource }</div>
+                                    </div>
                                 </div>
-                            </div>
-                })
-            }
-    	</div>
-  	);
+                    })
+                }
+            </div>
+          );
+    }
+  	
 };
 
 
